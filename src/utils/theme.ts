828e7259e3b70032310049b9cfdeccc5f222c728
volha <o.bukhal@gmail.com>
2022-05-06 09:43:44 +0300

    feat: add Header layout

---
+++ src/utils/theme.ts
@@ -0,0 +1,14 @@
+import { createTheme } from '@mui/material';
+export const theme = createTheme({
+  typography: {
+    fontSize: 20,
+  },
+  palette: {
+    primary: {
+      main: '#6585F3',
+    },
+    secondary: {
+      main: '#686970',
+    },
+  },
+});
