ee0329a97d7f0c8fb6faf468e27b1585c5adaf5d
volha <o.bukhal@gmail.com>
2022-05-06 22:31:39 +0300

    fix: remove condition render from BoardPage

--- src/components/pages/BoardsPage.tsx
+++ src/components/pages/BoardsPage.tsx
@@ -1,17 +1,10 @@
 import { Container, Button, Typography } from '@mui/material';
-import { useAppSelector } from '../../hooks/redux.hooks';
 
 const BoardsPage = () => {
-  const { isLoggedIn } = useAppSelector((state) => state.auth);
-
   return (
     <Container maxWidth="md" sx={{ mt: '1rem' }} data-testid="not-found-page">
-      {isLoggedIn && (
-        <>
-          <Typography> Existing board</Typography>
-          <Button>Create new board</Button>
-        </>
-      )}
+      <Typography> Existing board</Typography>
+      <Button>Create new board</Button>
     </Container>
   );
 };
