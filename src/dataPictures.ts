31e2a853a839d4ca4b3d3a2a03c5d706dc3340e7
Evgeniy <85104881+EVG777-prog@users.noreply.github.com>
2022-05-08 14:28:25 +0300

    add: file with array of backgrounds

---
+++ src/dataPictures.ts
@@ -0,0 +1,16 @@
+const dataPictures = [
+  'background0.jpg',
+  'background1.jpg',
+  'background2.jpg',
+  'background3.jpg',
+  'background4.jpg',
+  'background5.jpg',
+  'background6.jpg',
+  'background7.jpg',
+  'background8.jpg',
+  'background9.jpg',
+  'background10.jpg',
+  'background11.jpg',
+]
+
+export default dataPictures;
