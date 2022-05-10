92b6fd5759c3827680d1d8fef9ce7fc1862c9bd4
volha <o.bukhal@gmail.com>
2022-05-10 11:38:16 +0300

    feat: add notification if rejectMsg exist

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,12 +1,14 @@
 import { Outlet } from 'react-router-dom';
 import Header from '../Header';
 import Footer from '../Footer';
+import SnackbarMessage from '../SnackbarMessage';
 
 const Layout = () => {
   return (
     <>
       <Header />
       <Outlet />
+      <SnackbarMessage />
       <Footer />
     </>
   );
--- src/components/Snackbar/Snackbar.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const Snackbar = () => {
-  return <div>Snackbar</div>;
-};
-
-export default Snackbar;
