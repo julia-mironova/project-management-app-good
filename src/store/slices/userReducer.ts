96132f6397adb390a0a0a0659c65e9ba10d618ef
Evgeniy <85104881+EVG777-prog@users.noreply.github.com>
2022-05-16 03:55:23 +0300

    feat: show avatar user in task

---
+++ src/store/slices/userReducer.ts
@@ -0,0 +1,32 @@
+import { createAsyncThunk } from '@reduxjs/toolkit';
+import { BASE_URL } from '../../constants/baseUrl';
+import { localStorageGetUserToken } from '../../utils/localStorage';
+
+export type IUserResp = {
+  id: string;
+  name: string;
+  login: string;
+};
+
+export const getAllUsers = createAsyncThunk<IUserResp[], undefined, { rejectValue: string }>(
+  'board/getAllUsers',
+  async (_, { rejectWithValue }) => {
+    const token = localStorageGetUserToken();
+    const response = await fetch(`${BASE_URL}users`, {
+      method: 'GET',
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+
+    if (!response.ok) {
+      const resp = await response.json();
+      return rejectWithValue(
+        `bad server response, error code: ${resp?.statusCode} message: ${resp?.message}`
+      );
+    }
+
+    const resp: IUserResp[] = await response.json();
+    return resp;
+  }
+);
