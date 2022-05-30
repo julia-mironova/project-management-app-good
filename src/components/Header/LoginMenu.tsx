b2bc876d418c3743a8ee3008bc1d0e466d6d67b7
Viktor <ecartman41@gmail.com>
2022-05-30 21:44:02 +0300

    fix: style main buttom

--- src/components/Header/LoginMenu.tsx
+++ src/components/Header/LoginMenu.tsx
@@ -38,7 +38,11 @@ const LoginMenu = () => {
       {isLoggedIn ? (
         <Button
           onClick={() => navigate('/boards')}
+          variant="contained"
           sx={{
+            ml: 1,
+            backgroundColor: '#5c6bc0',
+            color: 'white',
             '@media only screen and (max-width: 450px)': {
               p: 0,
               m: 0.5,
