50769155fda4c97bb1ef721e460e01a06cb3331b
Viktor <ecartman41@gmail.com>
2022-05-30 20:18:54 +0300

    fix: add loading component

--- src/App.tsx
+++ src/App.tsx
@@ -7,11 +7,10 @@ import BoardsPage from './components/pages/BoardsPage';
 import RequireAuth from './hoc/RequireAuth';
 import ErrorBoundary from './components/ErrorBoundary/ErrorBoundary';
 import SignUp from './components/Login/SignUp';
-import { Backdrop, CircularProgress } from '@mui/material';
-import SingleBoardPage from './components/pages/SingleBoardPage';
+import Loading from './hoc/Loading';
 
 const Page404 = lazy(() => import('./components/pages/Page404'));
-//const SingleBoardPage = lazy(() => import('./components/pages/SingleBoardPage'));
+const SingleBoardPage = lazy(() => import('./components/pages/SingleBoardPage'));
 const EditProfile = lazy(() => import('./components/pages/EditProfile'));
 
 function App() {
@@ -25,13 +24,7 @@ function App() {
             element={
               <ErrorBoundary>
                 <RequireAuth>
-                  <Suspense
-                    fallback={
-                      <Backdrop sx={{ color: '#fff', zIndex: 100 }} open={true}>
-                        <CircularProgress color="inherit" />
-                      </Backdrop>
-                    }
-                  >
+                  <Suspense fallback={<Loading />}>
                     <EditProfile />
                   </Suspense>
                 </RequireAuth>
@@ -53,15 +46,9 @@ function App() {
             element={
               <ErrorBoundary>
                 <RequireAuth>
-                  {/*   <Suspense
-                    fallback={
-                      <Backdrop sx={{ color: '#fff', zIndex: 100 }} open={true}>
-                        <CircularProgress color="inherit" />
-                      </Backdrop>
-                    }
-                  > */}
-                  <SingleBoardPage />
-                  {/*   </Suspense> */}
+                  <Suspense fallback={<Loading />}>
+                    <SingleBoardPage />
+                  </Suspense>
                 </RequireAuth>
               </ErrorBoundary>
             }
